a11373923e9af86ef48ef6f43f11f72b7930a9cf
cairnsjr13 <cairnsjr13@gmail.com>
2023-11-30 22:35:19 -0800

    Add tests for 2018day01

--- src/main/java/com/cairns/rich/aoc/_2018/Base2018.java
+++ src/main/java/com/cairns/rich/aoc/_2018/Base2018.java
@@ -2,6 +2,10 @@ package com.cairns.rich.aoc._2018;
 
 import com.cairns.rich.aoc.Base;
 
+/**
+ * Base class for all advent of code puzzles from 2018.
+ * Because all of the actual days are package protected, the {@link #day} variable can be used to run explicit puzzles.
+ */
 public abstract class Base2018 extends Base {
   public static final Base day = new Day25();
 }
--- src/main/java/com/cairns/rich/aoc/_2018/Day01.java
+++ src/main/java/com/cairns/rich/aoc/_2018/Day01.java
@@ -5,12 +5,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * We need to calibrate our device.  By looking at the reported frequency changes we can get answers.
+ */
 class Day01 extends Base2018 {
+  /**
+   * Returns the final frequency after all of the input changes occur.
+   */
   @Override
   protected Object part1(Loader loader) {
     return loader.ml(Integer::parseInt).stream().mapToInt(Integer::intValue).sum();
   }
 
+  /**
+   * Returns the first frequency that is repeated when applying the input changes.
+   */
   @Override
   protected Object part2(Loader loader) {
     List<Integer> changes = loader.ml(Integer::parseInt);
