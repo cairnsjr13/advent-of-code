240d490ce52b2fcc7224672c982654cbcc0d38be
Rich Cairns <rcairns@rippling.com>
2025-01-06 09:21:33 -0700

    Add code and tests for 2024 day 20

---
+++ src/main/java/com/cairns/rich/aoc/_2024/Day20.java
@@ -0,0 +1,112 @@
+package com.cairns.rich.aoc._2024;
+
+import com.cairns.rich.aoc.EnumUtils;
+import com.cairns.rich.aoc.Loader;
+import com.cairns.rich.aoc.Loader.ConfigToken;
+import com.cairns.rich.aoc.grid.Grid;
+import com.cairns.rich.aoc.grid.ImmutablePoint;
+import com.cairns.rich.aoc.grid.ReadDir;
+import java.util.HashMap;
+import java.util.Map;
+
+class Day20 extends Base2024 {
+  private static final ConfigToken<Integer> cheatThresholdToken = ConfigToken.of("cheatThreshold", Integer::parseInt);
+
+  /**
+   * Cheats are allowed to move through exactly one '#' space (or a cheat distance of 2).
+   */
+  @Override
+  protected Object part1(Loader loader) {
+    return numImprovements(loader, 2);
+  }
+
+  /**
+   * Cheats are allowed to be at most a distance of 20.
+   */
+  @Override
+  protected Object part2(Loader loader) {
+    return numImprovements(loader, 20);
+  }
+
+  /**
+   * Returns the number of cheats that have an improvement at least as large as the configured threshold.
+   *
+   * At its core, a "cheat" is simply two open spaces ('.') that a racer can take a direct path to without worrying
+   * about blocked/open status.  The algorithm here considers every open space as a "start" position for a cheat
+   * and tries every open space within maxCheatDist as the "end" position.  The distance required using a cheat
+   * can be computed by adding together the best distance to the start of the cheat, the length of the cheat, and
+   * the best distance from the end of the cheat to the goal.
+   *
+   * We can dramatically reduce the runtime of this algorithm by limiting the end position search to having at
+   * least one of its directions in the threshold range.  While this is still larger than required because of the
+   * angled edges, it reduces the search range to dist*dist instead of the entire n*n grid.
+   */
+  private int numImprovements(Loader loader, int maxCheatDist) {
+    int cheatThreshold = loader.getConfig(cheatThresholdToken);
+    char[][] grid = loader.ml(String::toCharArray).toArray(char[][]::new);
+    ImmutablePoint start = Grid.findAndReplace(grid, 'S', '.');
+    ImmutablePoint end = Grid.findAndReplace(grid, 'E', '.');
+    Map<ImmutablePoint, Long> bestNoCheatFrom = computeBestNoCheatStartingAt(grid, end);
+    Map<ImmutablePoint, Long> bestNoCheatTo = computeBestNoCheatStartingAt(grid, start);
+    long bestNoCheat = bestNoCheatFrom.get(start);
+
+    int numCheatsAtOrAboveThreshold = 0;
+    for (int startRow = 1; startRow < grid.length - 1; ++startRow) {
+      for (int startCol = 1; startCol < grid[0].length - 1; ++startCol) {
+        ImmutablePoint startCheat = new ImmutablePoint(startCol, startRow);
+        if (isExpected(grid, startCheat, '.')) {
+          int minEndRow = Math.max(1, startRow - maxCheatDist);
+          int maxEndRow = Math.min(grid.length - 2, startRow + maxCheatDist);
+          int minEndCol = Math.max(1, startCol - maxCheatDist);
+          int maxEndCol = Math.min(grid[0].length - 2, startCol + maxCheatDist);
+          for (int endRow = minEndRow; endRow <= maxEndRow; ++endRow) {
+            for (int endCol = minEndCol; endCol <= maxEndCol; ++endCol) {
+              ImmutablePoint endCheat = new ImmutablePoint(endCol, endRow);
+              long cheatDist = Grid.manhattanDist(startCheat, endCheat);
+              if (cheatDist <= maxCheatDist) {
+                if (isExpected(grid, endCheat, '.')) {
+                  long timeWithCheat = bestNoCheatTo.get(startCheat)
+                                     + cheatDist
+                                     + bestNoCheatFrom.get(endCheat);
+                  if (bestNoCheat - timeWithCheat >= cheatThreshold) {
+                    ++numCheatsAtOrAboveThreshold;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return numCheatsAtOrAboveThreshold;
+  }
+
+  /**
+   * Using the given start point, computes a map of every reachable location from that point and the distance required.
+   */
+  private Map<ImmutablePoint, Long> computeBestNoCheatStartingAt(char[][] grid, ImmutablePoint from) {
+    Map<ImmutablePoint, Long> bestNoCheat = new HashMap<>();
+    bfs(
+        from,
+        (cur) -> false,
+        SearchState::getNumSteps,
+        (cur, numSteps, registrar) -> {
+          bestNoCheat.put(cur, numSteps);
+          for (ReadDir move : EnumUtils.enumValues(ReadDir.class)) {
+            ImmutablePoint next = cur.move(move);
+            if (Grid.isValid(grid, next) && isExpected(grid, next, '.')) {
+              registrar.accept(next);
+            }
+          }
+        }
+    );
+    return bestNoCheat;
+  }
+
+  /**
+   * Helper method to return if the given point in the grid has the given expected value.
+   */
+  private boolean isExpected(char[][] grid, ImmutablePoint at, char expect) {
+    return grid[at.y()][at.x()] == expect;
+  }
+}
--- src/main/java/com/cairns/rich/aoc/grid/Grid.java
+++ src/main/java/com/cairns/rich/aoc/grid/Grid.java
@@ -10,6 +10,42 @@ public final class Grid {
   private Grid() { /* Do not instantiate */ }
 
   /**
+   * Returns the Manhattan distance (horiz + vert) of two points.
+   * TODO: find other usages (search for Math.abs)
+   */
+  public static int manhattanDist(Point<?> first, Point<?> second) {
+    return Math.abs(first.x() - second.x())
+         + Math.abs(first.y() - second.y());
+  }
+
+  /**
+   * Returns the location of the given character in the grid.
+   * Throws a {@link RuntimeException} if not found.
+   * TODO: find other usages
+   */
+  public static ImmutablePoint find(char[][] grid, char target) {
+    for (int row = 0; row < grid.length; ++row) {
+      for (int col = 0; col < grid[0].length; ++col) {
+        if (grid[row][col] == target) {
+          return new ImmutablePoint(col, row);
+        }
+      }
+    }
+    throw new RuntimeException("Couldn't find '" + target + "'");
+  }
+
+  /**
+   * Returns the location of the given character in the grid after replacing it.
+   * Throws a {@link RuntimeException} if not found.
+   * TODO: find other usages
+   */
+  public static ImmutablePoint findAndReplace(char[][] grid, char target, char replace) {
+    ImmutablePoint found = find(grid, target);
+    grid[found.y()][found.x()] = replace;
+    return found;
+  }
+
+  /**
    * Returns true if the given grid contains the given point.
    */
   public static boolean isValid(List<List<?>> grid, Point<?> at) {
